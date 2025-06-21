e238c2f330fa53882aecb120599278a4323e1f02
msaakaash <msaakaash@hotmail.com>
2025-06-21 18:02:54 +0530

    Added Auth Controller

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import User from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const signUp = async (req,res,next) => {
+    try {
+        const { name: { firstName, lastName }, email, password, username } = req.body;
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({ success: false, message: 'User already exists.' });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = await User.create({
+            name: { firstName, lastName }, email, password:hashedPassword, username,
+        });
+
+        const token = jwt.sign(
+            {userId:newUser._id},
+            process.env.JWT_SECRET,
+            {expiresIn:process.env.JWT_EXPIRES_IN || '1h'}
+        );
+
+        res.status(201).json({
+             success:true,
+            message:"User created successfully",
+            data:{
+                token,
+                user:newUser,
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ success: false, message: 'Invalid email or password.' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ success: false, message: 'Invalid email or password.' });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN || '1h' }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: 'Login successful',
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    next(error); 
+  }
+};
