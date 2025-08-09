45d5ff37bbfad10d3e1f7b5327734be7cec99cb6
skr006 <karthikramtheskr@gmail.com>
2025-08-10 00:17:48 +0530

    Updated Profile.jsx with address form and input validation

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,19 +1,24 @@
 import Sidebar from "../components/Profilebar";
-import PageContent from "../components/PageContent";
 import { useState } from "react";
 import PersonalInfo from "../components/PersonalInfo";
 import Username from "../components/Username";
+import Name from "../components/Name";
+import Address from "../components/Address";
 const Profile = () => {
   const [category, setCategory] = useState(0);
 
   // Import your components for each category
 
   return (
-    <div className="flex h-screen py-20">
+    <div className="flex min-h-screen">
       <Sidebar setCategory={setCategory} />
-      {category === 0 && <PageContent />}
+      {category === 0 && <PersonalInfo />}
       {category === 1 && <PersonalInfo />}
-      {category === 2 && <Username />}
+      {category === 2 && <Name />}
+      {category === 3 && <Address />}
+      {category === 4 && <Username />}
+      {/* {category === 5 && <PhoneNumber />}
+      {category === 6 && <ChangePassword />} */}
     </div>
   );
 };
