f0ad8e1428f04fe8344cc6229634c2cfcfa02e6c
skr006 <karthikramtheskr@gmail.com>
2025-08-09 14:10:51 +0530

    Profile updations

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,15 +1,19 @@
 import Sidebar from "../components/Profilebar";
 import PageContent from "../components/PageContent";
 import { useState } from "react";
-
+import PersonalInfo from "../components/PersonalInfo";
+import Username from "../components/Username";
 const Profile = () => {
   const [category, setCategory] = useState(0);
+
+  // Import your components for each category
 
   return (
     <div className="flex h-screen py-20">
       <Sidebar setCategory={setCategory} />
-      <PageContent />
-      <p>{category}</p>
+      {category === 0 && <PageContent />}
+      {category === 1 && <PersonalInfo />}
+      {category === 2 && <Username />}
     </div>
   );
 };
